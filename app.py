f9365895b8e9f5b464db22ef3f1cfd3d654821ef
Maria Eduarda Vieira da Silva <dudav496@gmail.com>
2026-08-24 12:21:09 -0300

    template

--- app.py
+++ app.py
@@ -8,6 +8,15 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
