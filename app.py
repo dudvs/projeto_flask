1e27db88c21bdd888d467c8ca976c8c25eea7847
Maria Eduarda Vieira da Silva <dudav496@gmail.com>
2026-08-25 12:03:27 -0300

    atualização

--- app.py
+++ app.py
@@ -6,19 +6,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('dashboard/index.html')
+    return render_template('index.html')
 
 @app.route('/contato')
 def contato():
-    return render_template('dashboard/contato.html')
+    return render_template('contato.html')
 
 @app.route('/sobre')
 def sobre():
-    return render_template('dashboard/sobre.html')
+    return render_template('sobre.html')
 
 @app.route('/aluno')
 def lista_aluno():
-    return render_template('aluno/lista.html')
+    msg = 's senha é semsenha!'
+    return render_template('aluno/lista.html', texto=msg)
 
 @app.route('/professor')
 def lista_professor():
