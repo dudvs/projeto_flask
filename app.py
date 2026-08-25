87166316bebe608ac82bf80ed8f6892466ed8da8
Maria Eduarda Vieira da Silva <dudav496@gmail.com>
2026-08-25 11:19:39 -0300

    templates

--- app.py
+++ app.py
@@ -6,16 +6,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('dashboard/index.html')
 
 @app.route('/contato')
 def contato():
-    return render_template('contato.html')
-
+    return render_template('dashboard/contato.html')
 
 @app.route('/sobre')
 def sobre():
-    return render_template('sobre.html')
+    return render_template('dashboard/sobre.html')
 
 @app.route('/aluno')
 def lista_aluno():
